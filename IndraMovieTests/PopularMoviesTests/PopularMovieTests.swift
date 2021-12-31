3eb162b6c65f50bd1a742111346dd0a8168e48b6
Jordy Aguilar <houaguilar@gmail.com>
2021-12-31 00:00:44 -0500

    (feature) apply BDD

--- IndraMovieTests/PopularMoviesTests/PopularMovieTests.swift
+++ IndraMovieTests/PopularMoviesTests/PopularMovieTests.swift
@@ -14,40 +14,48 @@ import RxSwift
 class PopularMovieTests: QuickSpec {
 
     override func spec() {
-        it("return popular movies"){
-            var wrapperMovies: [WrapperMovie]!
-            let fetcher = StubPopularMoviesAPI()
-            let disposeBag = DisposeBag()
-            fetcher.getTwentyPopularMovies().subscribe(onNext: { event in
-                switch event {
-                case .success(let popularMovies):
-                    wrapperMovies = popularMovies
-                case .failure(let error):
-                    print(error.localizedDescription)
+        describe("getTwentyPopularMovies()", {
+            context("when get popular movies", {
+                it("return popular movies"){
+                    var wrapperMovies: [WrapperMovie]!
+                    let fetcher = StubPopularMoviesAPI()
+                    let disposeBag = DisposeBag()
+                    fetcher.getTwentyPopularMovies().subscribe(onNext: { event in
+                        switch event {
+                        case .success(let popularMovies):
+                            wrapperMovies = popularMovies
+                        case .failure(let error):
+                            print(error.localizedDescription)
+                        }
+                    }).disposed(by: disposeBag)
+                    expect(wrapperMovies).toEventuallyNot(beNil())
+                    expect(wrapperMovies?.count).toEventually(beGreaterThan(0))
                 }
-            }).disposed(by: disposeBag)
-            expect(wrapperMovies).toEventuallyNot(beNil())
-            expect(wrapperMovies?.count).toEventually(beGreaterThan(0))
-        }
-        it("fills movies data") {
-            var wrapperMovies: [WrapperMovie]!
-            let fetcher = StubPopularMoviesAPI()
-            let disposeBag = DisposeBag()
-            fetcher.getTwentyPopularMovies().subscribe(onNext: { event in
-                switch event {
-                case .success(let popularMovies):
-                    wrapperMovies = popularMovies
-                case .failure(let error):
-                    print(error.localizedDescription)
+            })
+            context("review prperties on popular movies", {
+                it("fills movies data") {
+                    var wrapperMovies: [WrapperMovie]!
+                    let fetcher = StubPopularMoviesAPI()
+                    let disposeBag = DisposeBag()
+                    fetcher.getTwentyPopularMovies().subscribe(onNext: { event in
+                        switch event {
+                        case .success(let popularMovies):
+                            wrapperMovies = popularMovies
+                        case .failure(let error):
+                            print(error.localizedDescription)
+                        }
+                    }).disposed(by: disposeBag)
+                    expect(wrapperMovies?[0].tmdb).toEventually(equal("634649"))
+                               expect(wrapperMovies?[0].title).toEventually(equal("Spider-Man: No Way Home"))
+                               expect(wrapperMovies?[0].year).toEventually(equal("2021-12-15"))
+                               expect(wrapperMovies?[1].tmdb).toEventually(equal("568124"))
+                               expect(wrapperMovies?[1].title).toEventually(equal("Encanto"))
+                               expect(wrapperMovies?[1].year).toEventually(equal("2021-11-24"))
                 }
-            }).disposed(by: disposeBag)
-            expect(wrapperMovies?[0].tmdb).toEventually(equal("634649"))
-                       expect(wrapperMovies?[0].title).toEventually(equal("Spider-Man: No Way Home"))
-                       expect(wrapperMovies?[0].year).toEventually(equal("2021-12-15"))
-                       expect(wrapperMovies?[1].tmdb).toEventually(equal("568124"))
-                       expect(wrapperMovies?[1].title).toEventually(equal("Encanto"))
-                       expect(wrapperMovies?[1].year).toEventually(equal("2021-11-24"))
-        }
+            })
+        })
+       
+        
     }
 
 }
